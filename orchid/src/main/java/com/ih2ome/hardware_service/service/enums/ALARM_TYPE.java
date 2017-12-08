3e2a7d29a0c7b4644704fd9d55aea5668d1172ee
xchfage <alpha.xu@ixiaoshuidi.com>
2017-12-08 19:41:22 +0800

    code review

--- orchid/src/main/java/com/ih2ome/hardware_service/service/enums/ALARM_TYPE.java
+++ orchid/src/main/java/com/ih2ome/hardware_service/service/enums/ALARM_TYPE.java
@@ -4,35 +4,28 @@ package com.ih2ome.hardware_service.service.enums;
  * <br>
  *
  * @author Lucius
- * create by 2017/12/7
+ *         create by 2017/12/7
  * @Emial Lucius.li@ixiaoshuidi.com
  */
 public enum ALARM_TYPE {
 
-    POWER_RATE_SMALL_THAN_ZERO("电费出现负数",0),
-    POWER_CONSUMPTION_WITHOUT_CHECKIN("未办理入住，有用电支出",1),
-    DATA_IS_NOT_UPDATE("数据未更新",2),
-    LONG_TIME_OFF_LINE("长时间设备离线",3),
-    POWER_NOT_FAILURE_WITH_POWER_RATE_THAN_ZERO("负数不断电",4);
+    POWER_RATE_SMALL_THAN_ZERO("电费出现负数", 0),
+    POWER_CONSUMPTION_WITHOUT_CHECKIN("未办理入住，有用电支出", 1),
+    DATA_IS_NOT_UPDATE("数据未更新", 2),
+    LONG_TIME_OFF_LINE("长时间设备离线", 3),
+    POWER_NOT_FAILURE_WITH_POWER_RATE_THAN_ZERO("负数不断电", 4);
 
 
     private String name;
     private Integer code;
 
-    public static ALARM_TYPE getByCode(Integer code){
-        if(code.equals(0)){
-            return ALARM_TYPE.POWER_RATE_SMALL_THAN_ZERO;
-        }else if(code.equals(1)){
-            return ALARM_TYPE.POWER_CONSUMPTION_WITHOUT_CHECKIN;
-        }else if(code.equals(2)){
-            return ALARM_TYPE.DATA_IS_NOT_UPDATE;
-        }else if(code.equals(3)){
-            return ALARM_TYPE.LONG_TIME_OFF_LINE;
-        }else if(code.equals(4)){
-            return ALARM_TYPE.POWER_NOT_FAILURE_WITH_POWER_RATE_THAN_ZERO;
+    public static ALARM_TYPE getByCode(Integer code) {
+        for (ALARM_TYPE alarmType : ALARM_TYPE.values()) {
+            if (alarmType.getCode().equals(code)) {
+                return alarmType;
+            }
         }
         return null;
-
     }
 
 
