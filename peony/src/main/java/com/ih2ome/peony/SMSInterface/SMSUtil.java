22d8e9c4cf8f4e7afb0589778fc94c4933bee080
“lucius <lucius.li@ixiaoshuidi.com>
2018-01-03 15:13:02 +0800

    门锁后台

---
+++ peony/src/main/java/com/ih2ome/peony/SMSInterface/SMSUtil.java
@@ -0,0 +1,31 @@
+package com.ih2ome.peony.SMSInterface;
+
+import com.alibaba.fastjson.JSONObject;
+import com.ih2ome.common.utils.HttpClientUtil;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * <br>
+ *
+ * @author Lucius
+ * create by 2018/1/3
+ * @Emial Lucius.li@ixiaoshuidi.com
+ */
+public class SMSUtil {
+
+    public static boolean sendTemplateText(String baseUrl,String templateCode
+            , String phoneNumber, JSONObject data, Integer interval ){
+        JSONObject req = new JSONObject();
+        req.put("number",phoneNumber);
+        req.put("template",templateCode);
+        req.put("data",data);
+        req.put("interval",interval);
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/json");
+        String result = HttpClientUtil.doPostJson(baseUrl+"/text", JSONObject.toJSONString(req),headers);
+        JSONObject res = JSONObject.parseObject(result);
+        return res.getString("retcode").equals("0");
+    }
+}
