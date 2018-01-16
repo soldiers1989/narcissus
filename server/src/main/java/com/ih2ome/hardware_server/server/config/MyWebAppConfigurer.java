b4ca403e6137a6473fe8c8698fbeb74a7b89f955
“lucius <lucius.li@ixiaoshuidi.com>
2018-01-16 11:39:08 +0800

    门锁后台

--- server/src/main/java/com/ih2ome/hardware_server/server/config/MyWebAppConfigurer.java
+++ server/src/main/java/com/ih2ome/hardware_server/server/config/MyWebAppConfigurer.java
@@ -1,9 +1,16 @@
 package com.ih2ome.hardware_server.server.config;
 
 import com.ih2ome.hardware_server.server.interceptor.OperateLogInterceptor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.nio.charset.Charset;
+import java.util.List;
 
 /**
  * <br>
@@ -14,6 +21,26 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
  */
 @Configuration
 public class MyWebAppConfigurer  extends WebMvcConfigurerAdapter {
+
+    @Bean
+    public HttpMessageConverter<String> responseBodyConverter() {
+        StringHttpMessageConverter converter = new StringHttpMessageConverter(
+                Charset.forName("UTF-8"));
+        return converter;
+    }
+
+    @Override
+    public void configureMessageConverters(
+            List<HttpMessageConverter<?>> converters) {
+        super.configureMessageConverters(converters);
+        converters.add(responseBodyConverter());
+    }
+
+    @Override
+    public void configureContentNegotiation(
+            ContentNegotiationConfigurer configurer) {
+        configurer.favorPathExtension(false);
+    }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry){
