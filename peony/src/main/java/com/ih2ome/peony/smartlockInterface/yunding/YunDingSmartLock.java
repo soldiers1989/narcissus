3cc59aa6e8aa41da0860228c4e9d0bda8b25b5a1
“lucius <lucius.li@ixiaoshuidi.com>
2018-01-23 16:03:15 +0800

    门锁后台

--- peony/src/main/java/com/ih2ome/peony/smartlockInterface/yunding/YunDingSmartLock.java
+++ peony/src/main/java/com/ih2ome/peony/smartlockInterface/yunding/YunDingSmartLock.java
@@ -41,7 +41,13 @@ public class YunDingSmartLock implements ISmartLock {
         return null;
     }
 
-    //新增密码
+    /**
+     * 新增密码
+     * @param lockPassword
+     * @return
+     * @throws SmartLockException
+     * @throws ParseException
+     */
     @Override
     public String addLockPassword(LockPasswordVo lockPassword) throws SmartLockException, ParseException {
         YunDingLockPasswordVO yunDingLockPasswordVO = YunDingLockPasswordVO.fromH2ome(lockPassword);
@@ -72,7 +78,13 @@ public class YunDingSmartLock implements ISmartLock {
         return result;
     }
 
-    //修改密码
+    /**
+     * 修改密码
+     * @param lockPassword
+     * @return
+     * @throws SmartLockException
+     * @throws ParseException
+     */
     @Override
     public String updateLockPassword(LockPasswordVo lockPassword) throws SmartLockException, ParseException {
         YunDingLockPasswordVO yunDingLockPasswordVO = YunDingLockPasswordVO.fromH2ome(lockPassword);
@@ -103,7 +115,13 @@ public class YunDingSmartLock implements ISmartLock {
         return result;
     }
 
-    //删除密码
+    /**
+     * 删除密码
+     * @param lockPassword
+     * @return
+     * @throws SmartLockException
+     * @throws ParseException
+     */
     @Override
     public String deleteLockPassword(LockPasswordVo lockPassword) throws SmartLockException, ParseException {
         YunDingLockPasswordVO yunDingLockPasswordVO = YunDingLockPasswordVO.fromH2ome(lockPassword);
@@ -130,7 +148,13 @@ public class YunDingSmartLock implements ISmartLock {
         return result;
     }
 
-    //冻结密码
+    /**
+     *  冻结密码
+     * @param lockPassword
+     * @return
+     * @throws SmartLockException
+     * @throws ParseException
+     */
     @Override
     public String frozenLockPassword(LockPasswordVo lockPassword) throws SmartLockException, ParseException {
         YunDingLockPasswordVO yunDingLockPasswordVO = YunDingLockPasswordVO.fromH2ome(lockPassword);
@@ -157,7 +181,13 @@ public class YunDingSmartLock implements ISmartLock {
         return result;
     }
 
-    //解冻结密码
+    /**
+     * 解冻结密码
+     * @param lockPassword
+     * @return
+     * @throws SmartLockException
+     * @throws ParseException
+     */
     @Override
     public String unfrozenLockPassword(LockPasswordVo lockPassword) throws SmartLockException, ParseException {
         YunDingLockPasswordVO yunDingLockPasswordVO = YunDingLockPasswordVO.fromH2ome(lockPassword);
@@ -184,7 +214,12 @@ public class YunDingSmartLock implements ISmartLock {
         return result;
     }
 
-    //查询所有的房源信息和设备信息
+    /**
+     * 查询所有的房源信息和设备信息
+     * @param params
+     * @return
+     * @throws SmartLockException
+     */
     @Override
     public String searchHomeInfo(Map<String, Object> params) throws SmartLockException {
         Log.info("查询该用户所有的房源和设备信息");
--- server/src/main/java/com/ih2ome/hardware_server/server/controller/constInfo/ConstInfoController.java
+++ server/src/main/java/com/ih2ome/hardware_server/server/controller/constInfo/ConstInfoController.java
@@ -1,8 +1,10 @@
 package com.ih2ome.hardware_server.server.controller.constInfo;
 
 import com.alibaba.fastjson.JSONObject;
+import com.ih2ome.common.api.enums.ApiErrorCodeEnum;
 import com.ih2ome.common.api.vo.request.ApiRequestVO;
 import com.ih2ome.common.base.BaseController;
+import com.ih2ome.common.utils.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
@@ -29,22 +31,25 @@ public class ConstInfoController extends BaseController{
     @Value("${yd.smart_lock.call_back.url}")
     String yunDingCallBackUrl;
 
-    final static String yunDingCallBackState = "success";
-
     @RequestMapping(value="/setAmmeterAlarmRules",method = RequestMethod.POST,produces = {"application/json"})
     public String getYunDingLoginPage(@RequestBody ApiRequestVO apiRequestVO){
-        StringBuilder url = new StringBuilder();
-        url.append(yunDingLoginBaseUrl)
-                .append("?client_id=")
-                .append(yunDingClientId)
-                .append("&redirect_uri=")
-                .append(yunDingCallBackUrl)
-                .append("&scope=")
-                .append(yunDingPermissionGroup)
-                .append("&state=")
-                .append(yunDingCallBackState);
-        JSONObject urlObject = new JSONObject();
-        urlObject.put("url",url);
-        return structureSuccessResponseVO(urlObject,new Date().toString(),"获取成功");
+        String userId = apiRequestVO.getDataRequestBodyVO().getDt().getString("userId");
+        if(StringUtils.isNotBlank(userId)){
+            StringBuilder url = new StringBuilder();
+            url.append(yunDingLoginBaseUrl)
+                    .append("?client_id=")
+                    .append(yunDingClientId)
+                    .append("&redirect_uri=")
+                    .append(yunDingCallBackUrl)
+                    .append("&scope=")
+                    .append(yunDingPermissionGroup)
+                    .append("&state=")
+                    .append(userId);
+            JSONObject urlObject = new JSONObject();
+            urlObject.put("url",url);
+            return structureSuccessResponseVO(urlObject,new Date().toString(),"获取成功");
+        }
+        return structureErrorResponse(ApiErrorCodeEnum.Service_request_geshi,new Date().toString(),"userId为空");
+
     }
 }
