c6cd3bcbfb8accb1420973ea159070a5c3413c7d
“lucius <lucius.li@ixiaoshuidi.com>
2018-01-26 17:02:48 +0800

    关联房源第三方接口

--- server/src/main/java/com/ih2ome/hardware_server/server/ShuidiApplication.java
+++ server/src/main/java/com/ih2ome/hardware_server/server/ShuidiApplication.java
@@ -2,6 +2,8 @@ package com.ih2ome.hardware_server.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
@@ -10,23 +12,23 @@ import org.springframework.context.annotation.ComponentScan;
  * create by 2017/10/30
  * @Emial Lucius.li@ixiaoshuidi.com
  */
-//@SpringBootApplication
-//@ComponentScan(basePackages = {"com.ih2ome"})
-//public class ShuidiApplication extends SpringBootServletInitializer {
-// @Override
-// protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-//  return application.sources(ShuidiApplication.class);
-// }
-// public static void main(String[] args) {
-//  SpringApplication.run(ShuidiApplication.class, args);
-// }
-//}
-
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.ih2ome"})
-public class ShuidiApplication{
-
-	public static void main(String[] args) {
-		SpringApplication.run(ShuidiApplication.class, args);
-	}
+public class ShuidiApplication extends SpringBootServletInitializer {
+ @Override
+ protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+  return application.sources(ShuidiApplication.class);
+ }
+ public static void main(String[] args) {
+  SpringApplication.run(ShuidiApplication.class, args);
+ }
 }
+
+//@SpringBootApplication
+//@ComponentScan(basePackages = {"com.ih2ome"})
+//public class ShuidiApplication{
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(ShuidiApplication.class, args);
+//	}
+//}
