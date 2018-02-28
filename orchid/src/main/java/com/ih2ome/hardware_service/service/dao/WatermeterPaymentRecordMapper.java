f421a4598dd42b0fb0125e1a145ad0acf08ce457
young <aspen.wang@ixiaoshuidi.com>
2018-02-28 19:07:54 +0800

    watermeter

--- orchid/src/main/java/com/ih2ome/hardware_service/service/dao/WatermeterPaymentRecordMapper.java
+++ orchid/src/main/java/com/ih2ome/hardware_service/service/dao/WatermeterPaymentRecordMapper.java
@@ -2,14 +2,13 @@ package com.ih2ome.hardware_service.service.dao;
 
 import com.ih2ome.common.base.MyMapper;
 import com.ih2ome.hardware_service.service.model.narcissus.SmartWatermeter;
-import com.ih2ome.hardware_service.service.model.narcissus.SmartWatermeterRecord;
 import com.ih2ome.hardware_service.service.model.narcissus.WatermeterPaymentRecord;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface WatermeterPaymentRecordMapper extends MyMapper<SmartWatermeterRecord> {
+public interface WatermeterPaymentRecordMapper extends MyMapper<WatermeterPaymentRecord> {
 
 
     List<SmartWatermeter> selectWatermeterByRoomId(Integer roomId);
