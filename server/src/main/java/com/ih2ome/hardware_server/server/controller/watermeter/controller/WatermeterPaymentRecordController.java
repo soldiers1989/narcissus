39ad86409e9b004bc5bda7e1f5ae8455041e7441
young <aspen.wang@ixiaoshuidi.com>
2018-03-01 15:35:18 +0800

    watermeter

--- server/src/main/java/com/ih2ome/hardware_server/server/controller/watermeter/controller/WatermeterPaymentRecordController.java
+++ server/src/main/java/com/ih2ome/hardware_server/server/controller/watermeter/controller/WatermeterPaymentRecordController.java
@@ -1,5 +1,6 @@
 package com.ih2ome.hardware_server.server.controller.watermeter.controller;
 
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.ih2ome.common.api.vo.request.ApiRequestVO;
 import com.ih2ome.common.base.BaseController;
@@ -51,25 +52,30 @@ public class WatermeterPaymentRecordController extends BaseController {
     @RequestMapping(value="/creatWatermeterPaymentRecord",method = RequestMethod.POST,produces = {"application/json"})
     public String creatWatermeterPaymentRecord(@RequestBody ApiRequestVO apiRequestVO)  {
         JSONObject dt = apiRequestVO.getDataRequestBodyVO().getDt();
-        Integer smartWatermeterId = dt.getIntValue("smartWatermeterId");
-        Integer lastNum = dt.getIntValue("lastNum");
-        Integer amount = dt.getIntValue("amount");
-        Long price = dt.getLongValue("price");
-        Date meterUpdateAt = dt.getDate("meterUpdateAt");
-        Integer meterType = dt.getIntValue("meterType");
-        WatermeterPaymentRecord watermeterPaymentRecord =new WatermeterPaymentRecord();
-        watermeterPaymentRecord.setSmartWatermeterId(smartWatermeterId);
-        watermeterPaymentRecord.setLastNum(lastNum);
-        watermeterPaymentRecord.setAmount(amount);
-        watermeterPaymentRecord.setPrice(price);
-        watermeterPaymentRecord.setMeterUpdateAt(meterUpdateAt);
-        watermeterPaymentRecord.setMeterType(meterType);
+        JSONArray paymentRecords = dt.getJSONArray("paymentRecord");
 
-        Boolean res= watermeterPaymentRecordService.createWatermeterPaymentRecord(watermeterPaymentRecord);
-        if (!res){
-            return "缴费记录创建失败";
+        for (int i=0;i<paymentRecords.size();i++){
+            JSONObject jsonObject = paymentRecords.getJSONObject(i);
+
+            Integer smartWatermeterId = jsonObject.getIntValue("smartWatermeterId");
+            Integer lastNum = jsonObject.getIntValue("lastNum");
+            Integer amount = jsonObject.getIntValue("amount");
+            Long price = jsonObject.getLongValue("price");
+            Date meterUpdateAt = jsonObject.getDate("meterUpdateAt");
+            Integer meterType = jsonObject.getIntValue("meterType");
+            WatermeterPaymentRecord watermeterPaymentRecord =new WatermeterPaymentRecord();
+            watermeterPaymentRecord.setSmartWatermeterId(smartWatermeterId);
+            watermeterPaymentRecord.setLastNum(lastNum);
+            watermeterPaymentRecord.setAmount(amount);
+            watermeterPaymentRecord.setPrice(price);
+            watermeterPaymentRecord.setMeterUpdateAt(meterUpdateAt);
+            watermeterPaymentRecord.setMeterType(meterType);
+
+            Boolean res= watermeterPaymentRecordService.createWatermeterPaymentRecord(watermeterPaymentRecord);
+            if (!res){
+                return "缴费记录创建失败";
+            }
         }
-
         return "success";
     }
 }
