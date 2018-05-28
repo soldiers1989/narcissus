ed9bf8dd5748f7b6a152f5e7e4328470f3560a6c
Hunter Pan <hunter.pan@ixiaoshuidi.com>
2018-05-28 18:02:17 +0800

    add log

--- server/src/main/java/com/ih2ome/hardware_server/server/callback/help/YunDingCallBackHelp.java
+++ server/src/main/java/com/ih2ome/hardware_server/server/callback/help/YunDingCallBackHelp.java
@@ -124,7 +124,8 @@ public class YunDingCallBackHelp {
             watermeterRecordService.addWatermeterRecord(smartWatermeterRecord);
             //计算金额
             if (amount > watermeter.getLastAmount()) {
-                watermeterService.changeBalance(Integer.parseInt(device.getRoomId()), Integer.parseInt(device.getHouseCatalog()), (amount - watermeter.getLastAmount()) * watermeter.getPrice(), "", "system_async", "");
+                Log.warn("第三方返回电表读数大于最近读数，将进行扣费 deviceId:{};返回读数:{};最近读数:{};单价:{}", watermeter.getSmartWatermeterId(), amount, watermeter.getLastAmount(), watermeter.getPrice());
+                watermeterService.changeBalance(Integer.parseInt(device.getRoomId()), Integer.parseInt(device.getHouseCatalog()), (amount - watermeter.getLastAmount()) * watermeter.getPrice() * -1, "", "system_async", "");
             }
             if (amount < watermeter.getLastAmount()) {
                 Log.warn("第三方返回电表读数小于最近读数，请关注！ deviceId:{};返回读数:{};最近读数:{}", watermeter.getSmartWatermeterId(), amount, watermeter.getLastAmount());
