3987706fbf2c9fb6b38046d126cd288b3e72d355
wang <porter.wang@ixiaoshuidi.com>
2018-06-08 09:58:37 +0800

    Saas

--- orchid/src/main/java/com/ih2ome/hardware_service/service/service/SaasSmartLockService.java
+++ orchid/src/main/java/com/ih2ome/hardware_service/service/service/SaasSmartLockService.java
@@ -1,6 +1,11 @@
 package com.ih2ome.hardware_service.service.service;
 
 import com.ih2ome.sunflower.model.backup.SaasSmartLock;
+import com.ih2ome.sunflower.vo.pageVo.Ammeter;
+import com.ih2ome.sunflower.vo.pageVo.smartLock.SmartLockDetailVO;
+import com.ih2ome.sunflower.vo.pageVo.watermeter.WatermeterDetailVO;
+
+import java.util.List;
 
 
 public interface SaasSmartLockService {
@@ -13,4 +18,10 @@ public interface SaasSmartLockService {
     SaasSmartLock getSmartLock(String userId, String type, String roomId);
 
     SaasSmartLock getSmartLockCount(String userId,String type);
+
+    SmartLockDetailVO getInformation(String type, String roomId);
+
+    List<WatermeterDetailVO> getWatermeter(String type, String roomId);
+
+    Ammeter getAmmeter(String type, String roomId);
 }
--- server/src/main/java/com/ih2ome/hardware_server/server/controller/smartlock/SaasLockController.java
+++ server/src/main/java/com/ih2ome/hardware_server/server/controller/smartlock/SaasLockController.java
@@ -3,11 +3,15 @@ package com.ih2ome.hardware_server.server.controller.smartlock;
 import com.alibaba.fastjson.JSONObject;
 import com.ih2ome.hardware_service.service.service.SaasSmartLockService;
 import com.ih2ome.sunflower.model.backup.SaasSmartLock;
+import com.ih2ome.sunflower.vo.pageVo.Ammeter;
+import com.ih2ome.sunflower.vo.pageVo.smartLock.SmartLockDetailVO;
+import com.ih2ome.sunflower.vo.pageVo.watermeter.WatermeterDetailVO;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
+import java.util.List;
 
 import static com.ih2ome.common.base.BaseController.structureSuccessResponseVO;
 
@@ -56,6 +60,19 @@ public class SaasLockController {
             jsonObject.put("count",count.getCount());
             jsonObject.put("gatway",count.getGatWay());
         }
+        String result =structureSuccessResponseVO(jsonObject,new Date().toString(),"查询成功");
+        return result;
+    }
+    @GetMapping(value = "/search/information")
+    public String getSmartInformation(@RequestParam  String type,
+                                        @RequestParam  String roomId){
+        SmartLockDetailVO smartLockDetailVO=saasSmartLockService.getInformation(type,roomId);
+        List<WatermeterDetailVO> watermeterDetailVO=saasSmartLockService.getWatermeter(type,roomId);
+        Ammeter Ammeter=saasSmartLockService.getAmmeter(type,roomId);
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("SmartLockDetailVO",smartLockDetailVO);
+        jsonObject.put("watermeterDetailVO",watermeterDetailVO);
+        jsonObject.put("Ammeter",Ammeter);
         String result =structureSuccessResponseVO(jsonObject,new Date().toString(),"查询成功");
         return result;
     }
